75dbf7dd7f35869660f014ee52599885708d31c7
banelmikhael <banel.mikhael.bm@gmail.com>
2022-10-11 01:20:23 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1=1
+let num2=62
+let multiply=num1*num2
 
-// Write your code here
+
+let num3=4
+let num4=16
+let mod = num3%num4
+
+let infi ;
+
+let random=Math.random() * infi | 1
+
+let min = 1;
+let max=20;
+let MAX = Math.random() * (max - min) + min;
+console.log(randomNum);
+
